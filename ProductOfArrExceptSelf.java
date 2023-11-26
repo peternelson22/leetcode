51c18dff2b382df8ecefdd3733717f3150be70f6
nelson <peternelson063@gmail.com>
2023-11-26 22:13:50 +0100

    fix: ProductOfArrayExceptSelf

--- ProductOfArrExceptSelf.java
+++ ProductOfArrExceptSelf.java
@@ -3,24 +3,22 @@ import java.util.*;
 
 class ProductOfArrExceptSelf{
     public static int[] productOfArr(int[] nums){
-        int n = nums.length;
+         int n = nums.length;
 
         int[] result = new int[n];
-        
-        int temp = 1;
+
+        int leftProduct = 1;
         for(int i=0; i<n; i++){
-            result[i] = temp;
-            temp *= nums[i];
+            result[i] = leftProduct;
+            leftProduct *= nums[i];
         }
-
-        temp = 1;
+        int rightProduct = 1;
         for(int i=n-1; i>=0; i--){
-            result[i] = temp;
-            temp *= nums[i];
+            result[i] *= rightProduct;
+            rightProduct *= nums[i];
         }
-
         return result;
-    } 
+    }
      public static void main(String[] args){
          int[] p = {1,2,3,4};
          System.out.println(Arrays.toString(productOfArr(p))); 
